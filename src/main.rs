c807480d4196e2cb6e20c8ec172d8d47378292f9
Leon Bentrup <leon@leonadi.de>
2017-01-31 19:16:47 +0100

    Add json serialization

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
 mod message;
 mod error;
 extern crate chrono;
+/*
+extern crate mqtt3;
+extern crate netopt;
+extern crate mqttc;
+*/
+extern crate rustc_serialize;
 
 use std::net::TcpStream;
 use std::io::{BufRead, BufReader};
+//use std::time::Duration;
+use rustc_serialize::json::ToJson;
 
 
 fn main() {
-    let stream = TcpStream::connect("10.0.0.1:1012").unwrap();
+    /*
+    let netopts = netopt::NetworkOptions::new();
+    let mut mqtt_opts = mqttc::ClientOptions::new();
+    mqtt_opts.set_reconnect(mqttc::ReconnectMethod::ReconnectAfter(Duration::from_secs(1)));
+    let mut mqtt_client = mqtt_opts.connect("iot.eclipse.org:1883", netopts).expect("Error connecting to server");
+    */
+
+    let stream = TcpStream::connect("127.0.0.1:1012").unwrap();
     let mut reader = BufReader::new(stream);
     loop {
         let mut line = String::new();
         let result = reader.read_line(&mut line);
         match result {
             Ok(_) => {
-                let m = message::Message::parse_from_str(&line);
-                println!("{:?}", m);
+                match message::Message::parse_from_str(&line) {
+                    Ok(m) => { println!("{}", m.to_json()); }
+                    Err(e) => { println!("Error: {:?}", e); }
+                }
+
             }
             Err(e) => {
                 println!("Err: {}", e);
--- src/message.rs
+++ src/message.rs
@@ -2,6 +2,8 @@ use chrono::NaiveDateTime;
 use chrono;
 use error::Error;
 use std::str::FromStr;
+use rustc_serialize::json::{ToJson, Json};
+use std::collections::BTreeMap;
 
 #[derive(Debug)]
 enum Event {
@@ -42,6 +44,45 @@ impl Event {
 
         };
         Ok(event)
+    }
+}
+
+impl ToJson for Event {
+    fn to_json(&self) -> Json {
+        let mut d = BTreeMap::new();
+        match *self {
+            Event::Call {ref extension, ref from, ref to} => {
+                d.insert("type".to_string(), "call".to_json());
+                d.insert("extension".to_string(), extension.to_json());
+                d.insert("from".to_string(), from.to_json());
+                d.insert("to".to_string(), to.to_json());
+            }
+            Event::Ring { ref from, ref to} => {
+                d.insert("type".to_string(), "ring".to_json());
+                d.insert("from".to_string(), from.to_json());
+                d.insert("to".to_string(), to.to_json());
+            }
+            Event::Connect {ref extension, ref from} => {
+                d.insert("type".to_string(), "connect".to_json());
+                d.insert("extension".to_string(), extension.to_json());
+                d.insert("from".to_string(), from.to_json());
+            }
+            Event::Disconnect {ref duration} => {
+                d.insert("type".to_string(), "disconnect".to_json());
+                d.insert("duration".to_string(), duration.to_json());
+            }
+        };
+        Json::Object(d)
+    }
+}
+
+impl ToJson for Message {
+    fn to_json(&self) -> Json {
+        let mut d = BTreeMap::new();
+        d.insert("timestamp".to_string(), self.timestamp.to_string().to_json());
+        d.insert("connectionId".to_string(), self.connection_id.to_json());
+        d.insert("event".to_string(), self.event.to_json());
+        Json::Object(d)
     }
 }
 
